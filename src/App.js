2b78cb50fc8fe309e5bab83129fd658f3f1c8c87
amsohag007 <amsohag007@gmail.com>
2020-09-19 06:11:48 +0600

    firebase user authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import { db } from './firebase';
+import { auth, db } from './firebase';
 import Post from './Post';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
@@ -30,16 +30,37 @@ const useStyles = makeStyles((theme) => ({
 
 
 function App() {
+  //modal-signUp
   const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
   const [open,setOpen]=useState(false);
+  const [openSignIn,setOpenSignIn]=useState(false);
+
 
   const [posts, setPosts] = useState([]);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [user,setUser]=useState(null);
   
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        //user has logged in
+        console.log(authUser);
+        setUser(authUser);
+
+      }else{
+        //user has logged out
+        setUser(null)
+      }
+    }) 
+
+    return () => {
+      //cleanup action
+      unsubscribe();  
+    }
+  }, [user,username])//any time they change reload
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot=>{
@@ -52,9 +73,28 @@ function App() {
     })
   }, []) 
 
+  //firebase user authentication
   const signUp=(e)=>{
     e.preventDefault();
+    auth.createUserWithEmailAndPassword(email,password)
+        .then((authUser)=>{
+          return authUser.user.updateProfile({
+            displayName:username
+          })
+        })
+        .catch((error)=>alert(error.message));
 
+    setOpen(false);
+
+  }
+  const signIn = (e) => {
+    e.preventDefault();
+    
+    auth
+     .signInWithEmailAndPassword(email,password)
+     .catch((error)=>alert(error.message))
+    
+    setOpenSignIn(false);
   }
 
 
@@ -75,12 +115,46 @@ function App() {
               alt="instsgram logo"
               />
             </center>
-              <Input 
+            <Input 
                 placeholder="username"
                 type="text"
                 value={username}
                 onChange={(e)=>setUsername(e.target.value)}
+            />
+            <Input 
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+            /> 
+            <Input 
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+           
+          </form>
+          
+         </div>
+      </Modal>
+      {/* //signIn modal */}
+      <Modal
+        open={openSignIn}
+        onClose={()=>setOpenSignIn(false)}
+      >
+         <div style={modalStyle} className={classes.paper}>
+
+          <form className="app__signUp">
+            <center>
+              <img 
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt="instsgram logo"
               />
+            </center>
+              
               <Input 
                 placeholder="email"
                 type="text"
@@ -93,7 +167,7 @@ function App() {
                 value={password}
                 onChange={(e)=>setPassword(e.target.value)}
               />
-              <Button type="submit" onClick={signUp}>Sign Up</Button>
+              <Button type="submit" onClick={signIn}>Sign In</Button>
            
           </form>
           
@@ -106,7 +180,15 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="instsgram logo"
         />
-        <Button onClick={()=>setOpen(true)}>Sign Up</Button>
+        {user?(
+          <Button onClick={()=>auth.signOut()}>Log Out</Button>
+          ):(
+            <div className="app__loginConatainer">
+              <Button onClick={()=>setOpenSignIn(true)}>Sign In</Button>
+              <Button onClick={()=>setOpen(true)}>Sign Up</Button>
+            </div>
+          
+        )}
       </div>
       
       {
